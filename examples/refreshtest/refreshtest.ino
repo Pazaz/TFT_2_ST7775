e6118e495a6e78d73264f4704a73b9468517f1f4
Jonathan Steele <ItsPazaz@gmail.com>
2015-09-21 23:43:36 -0400

    Basic example to test performance

---
+++ examples/refreshtest/refreshtest.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_GFX.h>
+#include <TFT_2_ST7775.h>
+
+ST7775::Display display(11, 10, 12);
+
+void setup() {
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  display.setPins(9, 8, 7, 6, 5, 4, 3, 2);
+  display.begin();
+}
+
+void loop() {
+  static int currentTime = millis(), lastTime = 0;
+  display.fillRect(0, 0, ST7775::WIDTH, ST7775::HEIGHT, ST7775::Utility::RGB_565(random(), random(), random()));
+  lastTime = currentTime;
+  currentTime = millis();
+  Serial.print("Took "); Serial.print(currentTime - lastTime); Serial.println("ms to render 176x220 pixels");
+}
+
