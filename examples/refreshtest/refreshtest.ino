1be08cc98fb77b4c07a6f058330ae96b261d906b
Jonathan Steele <ItsPazaz@gmail.com>
2015-09-22 02:03:52 -0400

    PROGMEM used for literal strings.

--- examples/refreshtest/refreshtest.ino
+++ examples/refreshtest/refreshtest.ino
@@ -4,17 +4,16 @@
 ST7775::Display display(11, 10, 12);
 
 void setup() {
-  Serial.begin(9600);
-  randomSeed(analogRead(0));
-  display.setPins(9, 8, 7, 6, 5, 4, 3, 2);
-  display.begin();
+	Serial.begin(9600);
+	randomSeed(analogRead(0));
+	display.setPins(9, 8, 7, 6, 5, 4, 3, 2);
+	display.begin();
 }
 
 void loop() {
-  static int currentTime = millis(), lastTime = 0;
-  display.fillRect(0, 0, ST7775::WIDTH, ST7775::HEIGHT, ST7775::Utility::RGB_565(random(), random(), random()));
-  lastTime = currentTime;
-  currentTime = millis();
-  Serial.print("Took "); Serial.print(currentTime - lastTime); Serial.println("ms to render 176x220 pixels");
+	static int currentTime = millis(), lastTime = 0;
+	display.fillRect(0, 0, ST7775::WIDTH, ST7775::HEIGHT, ST7775::Utility::RGB_565(random(), random(), random()));
+	lastTime = currentTime;
+	currentTime = millis();
+	Serial.print(F("Took ")); Serial.print(currentTime - lastTime); Serial.println(F("ms to render 176x220 pixels"));
 }
-
